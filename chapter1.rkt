7f539ae821a09c18665cbb5f0dc4cb00c1509a97
Thomas Barabosch <thomas.barabosch@gmail.com>
2018-02-23 22:52:02 +0100

    Exercise 1.7

--- chapter1.rkt
+++ chapter1.rkt
@@ -12,3 +12,27 @@
   (cond ((and (= (max2 a b) a) (= (max2 b c) c)) (sum-square a c))
 	((and (= (max2 a b) b) (= (max2 a c) c)) (sum-square b c))
 	(else (sum-square a b))))
+
+;; Exercise 1.7
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (improve-guess guess x)
+  (average guess (/ x guess)))
+
+;; should work due to the limited precision of floating point numbers
+(define (good-enough? guess x)
+ (= guess (improve-guess guess x)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve-guess guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
